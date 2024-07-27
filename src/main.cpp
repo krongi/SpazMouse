a22bfbe9fd26a03306b196be741d207bc7d7ea58
krongi <mgseverson83@gmail.com>
2024-07-26 22:14:46 -0400

    Initial push to github. A random movement generator for esp32. Moves mouse randomly every 5 seconds.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <BleMouse.h>
 #include <BleKeyboard.h>
-// #include <BLEScan.h>
 #include <BLEDevice.h>
 #include <math.h>
 
@@ -15,17 +14,10 @@ BleKeyboard bkey;
 
 void setup() {
   // put your setup code here, to run once:
-  // bluedev.getScan();
-  // bluedev.getScan();
-  int rando = rand();
-  Serial.print(rando);
   bmouse.deviceName = "MX Vertical";
   bmouse.deviceManufacturer = "Logitech";
-  // bkey.setName("Long Dick Keyboard");
   Serial.begin(115200);
   Serial.println("Starting Mouse");
-  
-  // bkey.begin();
   bmouse.begin();
 }
 
@@ -34,18 +26,10 @@ void loop() {
   int y = random(0, 4000);
   // put your main code here, to run repeatedly:
   if(bmouse.isConnected()){
-    Serial.println("Working");
     bmouse.move(x, y);
-    Serial.print("Mouse moved to ");
-    Serial.print(x);
-    Serial.print(", ");
-    Serial.println(y);
+
   }
-  if(bkey.isConnected()){
-    Serial.println("Keybaord connected you dumb fuck");
-  }
-  Serial.println("Begin 5 Second Wait");
-  
+
   delayMicroseconds(5000000);
   
 }
